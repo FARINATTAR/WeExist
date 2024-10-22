7beb300e9706ac17a33c79bec587a3e2ae62f67e
Farin <farinattar99@gmail.com>
2024-10-22 21:20:18 +0530

    Added dropdown toggle for 'Get Involved' and arrow indicator in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,24 @@ function Header() {
     return (
         <header className="header">
             <div className="logo">
-                <img src={logo} alt="Logo of WeExist"  />
+                <img src={logo} alt="Logo of WeExist" />
             </div>
-             
+
             <nav className="nav">
                 <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About Us</a></li>
                     <li><a href="#work">Our Work</a></li>
-                    <li><a href="#involved">Get Involved</a></li>
+                    <li className="dropdown">
+                        <a href="#involved" className="dropbtn">
+                            Get Involved <span className="arrow">▼</span>
+                        </a>
+                        <div className="dropdown-content">
+                            <a href="#volunteer">Volunteer</a>
+                            <a href="#donate">Donate</a>
+                            <a href="#partner">Partner with Us</a>
+                        </div>
+                    </li>
                     <li><a href="#contact">Contact Us</a></li>
                     <li><a href="#faqs">FAQs</a></li>
                 </ul>
@@ -25,7 +34,6 @@ function Header() {
                 <span className="pipe">|</span>
                 <a href="#donate" className="btn donate-btn">Donate</a>
             </div>
-
         </header>
     );
 }
